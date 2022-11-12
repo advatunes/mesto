3082ef07a8542a3c3d9e1777cf5393ec1c1c097a
advatunes <lapostx@gmail.com>
2022-11-12 19:46:06 +0300

    add profile buttonedit

--- blocks/script/index.js
+++ blocks/script/index.js
@@ -1,31 +1,38 @@
 let popupElement = document.querySelector(".popup"),
+  popupForm = popupElement.querySelector(".popup__container"),
   popupOpenButton = document.querySelector(".profile__edit-button"),
   popupCloseButton = popupElement.querySelector(".popup__close-icon"),
   nameInput = popupElement.querySelector(".popup__name"),
   jobInput = popupElement.querySelector(".popup__job"),
   popupSaveButton = popupElement.querySelector(".popup__submit"),
   profileName = document.querySelector(".profile__name"),
-  profileJob = document.querySelector(".profile__job");
+  profileJob = document.querySelector(".profile__job"),
+  likeButton = document.querySelectorAll(".element__like");
 
 function popupOpen() {
-  popupElement.classList.add('popup_opened');
+  popupElement.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function popupClose() {
-  popupElement.classList.remove('popup_opened');
+  popupElement.classList.remove("popup_opened");
 }
 
-popupOpenButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupClose);
+popupOpenButton.addEventListener("click", popupOpen);
+popupCloseButton.addEventListener("click", popupClose);
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
-
-popupSaveButton.addEventListener('submit', function(evt) {
-  evt.preventDefault();
+popupForm.addEventListener("submit", function (evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  popupElement.classList.remove("popup_opened");
+  evt.preventDefault();
 });
 
+// function addLike() {
+//   likeButton.classList.add("element__like_active");
+// }
 
+likeButton.addEventListener("click", function addLike() {
+  likeButton.classList.add("element__like_active");
+});
