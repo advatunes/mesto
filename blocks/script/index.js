975e394cfa20d778150a315e609445ee1692dd1d
Dmitriy Nik <dj-9-1-1@yandex.ru>
2022-11-13 20:33:47 +0300

    fix 1review

--- blocks/script/index.js
+++ blocks/script/index.js
@@ -1,30 +1,32 @@
-let popupElement = document.querySelector(".popup"),
-  popupForm = popupElement.querySelector(".popup__container"),
-  popupOpenButton = document.querySelector(".profile__edit-button"),
-  popupCloseButton = popupElement.querySelector(".popup__close-icon"),
-  nameInput = popupElement.querySelector(".popup__name"),
-  jobInput = popupElement.querySelector(".popup__job"),
-  profileName = document.querySelector(".profile__name"),
-  profileJob = document.querySelector(".profile__job");
+let popupElement = document.querySelector('.popup'),
+  popupForm = popupElement.querySelector('.popup__form'),
+  popupOpenButton = document.querySelector('.profile__edit-button'),
+  popupCloseButton = popupElement.querySelector('.popup__close-icon'),
+  nameInput = popupElement.querySelector('.popup__input_name'),
+  jobInput = popupElement.querySelector('.popup__input_job'),
+  profileName = document.querySelector('.profile__name'),
+  profileJob = document.querySelector('.profile__job');
 
-function popupOpen() {
-  popupElement.classList.add("popup_opened");
+// Открытие попапа
+function openPopup() {
+  popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function popupClose() {
-  popupElement.classList.remove("popup_opened");
+// Закрытие попапа
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener("click", popupOpen);
-popupCloseButton.addEventListener("click", popupClose);
-
-popupForm.addEventListener("submit", function (evt) {
+// Копирование текстовых полей попапа в профиль
+function editProfileValue(evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupElement.classList.remove("popup_opened");
+  closePopup();
   evt.preventDefault();
-});
+}
 
-
+popupForm.addEventListener('submit', editProfileValue);
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
