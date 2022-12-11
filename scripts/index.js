5f5ffa0e126d18c58c1ef9d15436dd1b823e1a5b
Dmitriy Nik <dj-9-1-1@yandex.ru>
2022-12-11 15:56:19 +0300

    js add close popup by overlay click

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,16 @@ const popupNameElement = document.querySelector('.popup_name'),
 // Открытие попапа
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
-  document.addEventListener('keyup', handleKeyDown);
+  document.addEventListener('keydown', handleKeyDown);
+  popup.addEventListener('click', handleOutsideClick);
+
 };
 
 // Закрытие попапа
 const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('keyup', handleKeyDown);
+  document.removeEventListener('keydown', handleKeyDown);
+  popup.removeEventListener('click', handleOutsideClick);
 };
 
 // Кнопки откр/закр профиля
@@ -59,6 +62,16 @@ const handleKeyDown = (e) => {
     closePopup(popup);
   };
 };
+
+// Закрытие попапа по клику
+
+const handleOutsideClick = (e) => {
+  if (!e.target.closest('.popup__container')) {
+     closePopup(e.target);
+  };
+};
+
+
 
 // Перенос текстовых полей
 nameInput.value = profileName.textContent;
@@ -136,76 +149,3 @@ const addCard = (e) => {
 
 popupPlaceForm.addEventListener('submit', addCard);
 
-const config = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit',
-  inactiveButtonClass: 'popup__submit_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible',
-};
-
-// Проверка валидности
-
-const hasValidInput = (inputList) => {
-  return inputList.every((inputElement) => inputElement.validity.valid);
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-// Включение и отключение submit
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasValidInput(inputList)) {
-    buttonElement.classList.remove(config.inactiveButtonClass);
-    buttonElement.disabled = '';
-  } else {
-    buttonElement.classList.add(config.inactiveButtonClass);
-    buttonElement.disabled = 'disabled';
-  }
-};
-
-// Отображение ошибки
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(config.errorClass);
-  inputElement.classList.add(config.inputErrorClass);
-};
-
-// Скрытие ошибки
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.classList.remove(config.errorClass);
-  errorElement.textContent = '';
-};
-
-// Добавление обработчиков
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = (config) => {
-  const { formSelector, ...rest } = config;
-  const formList = Array.from(document.querySelectorAll(formSelector));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (e) => e.preventDefault());
-    setEventListeners(formElement);
-  });
-};
-
-enableValidation(config);
